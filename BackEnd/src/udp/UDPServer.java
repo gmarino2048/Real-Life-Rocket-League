22a5374ef6065ff675be35d1a86e0d61b5579889
Nikhil <nsc27@case.edu>
2018-02-28 18:52:46 -0500

    demo udp

--- BackEnd/src/udp/UDPServer.java
+++ BackEnd/src/udp/UDPServer.java
@@ -16,12 +16,17 @@ public class UDPServer {
 		DatagramPacket pack;
 		
 		try {
-			serv = new DatagramSocket();
+			serv = new DatagramSocket(3006);
 			while(alive) {
 				pack = new DatagramPacket(buff, buff.length);
 				serv.receive(pack);
-				
-				
+				InetAddress add = pack.getAddress();
+				int port = pack.getPort();
+				pack = new DatagramPacket(buff, buff.length, add, port);
+				String received = new String(pack.getData(),0,pack.getLength());
+				System.out.println(received);
+				if(received.contains("end"))
+					alive = false;
 			}
 			serv.close();
 		} catch (SocketException e) {
