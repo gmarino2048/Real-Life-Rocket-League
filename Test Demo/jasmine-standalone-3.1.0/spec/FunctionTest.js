8682d98f6afdb93f9554f2d6d61c6e521e3f07c2
BuildTools <gxm262@case.edu>
2018-04-01 11:44:36 -0400

    Test demo for Brie

---
+++ Test Demo/jasmine-standalone-3.1.0/spec/FunctionTest.js
@@ -0,0 +1,13 @@
+
+describe ("myFunctionName", function() {
+
+  it("should return one if number is 1", function(){
+    expect(myFunctionName(1)).toEqual("one");
+  });
+
+  it("should return two if number is 2", function () {
+    expect(myFunctionName(2)).toEqual("two");
+  });
+
+
+});
