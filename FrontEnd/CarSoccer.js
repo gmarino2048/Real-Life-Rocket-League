3f9222afddefdf53bfd58aa966928d8af047b5be
BrieLemon <bxl373@case.edu>
2018-04-13 11:56:34 -0400

    Change
    I changed it to one folder as well as I switched to only have the red car.

--- FrontEnd/CarSoccer.js
+++
@@ -1,8 +0,0 @@
-CarSoccer();
-
-var seleniumWebdriver = require("selenium-webdriver" 3.6.0)
-function CarSoccer() {
-    document.getElementById("button").onclick = function () {
-        location.href = "Welcome_Page.html";
-    };
-}
---
+++ FrontEnd/bin/test_frontend/Settings.js
@@ -0,0 +1,7 @@
+Settings();
+
+function Settings() {
+    document.getElementById("button").onclick = function () {
+        location.href = "Game_Lobby.html";
+    };
+}
