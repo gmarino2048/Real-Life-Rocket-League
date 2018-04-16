eb015c0441e0c53243fd2238f9b05fb8f807ef13
Guy Marino <gxm262@case.edu>
2018-04-16 16:48:52 -0400

    Finally uploading the code for the arena controller

--- Hardware/Arena/Arduino/Arena-Controller/Arena-Controller.ino
+++ Hardware/Arena/Arduino/Arena-Controller/Arena-Controller.ino
@@ -2,7 +2,7 @@
  *  
  *  v 1.0
  *  
- *  SPEC - 04
+ *  Arena-Controller
  *  
  * The purpose of this test is to ensure that the particle photon
  * can correctly connect to the arduino due and that the due can
@@ -11,6 +11,8 @@
  *  
  *  This script was written for the Arduino DUE
  */
+
+// Data transfer variables
 String infoBuffer;
 
 String variableKey;
@@ -24,46 +26,30 @@ String username2;
 
 int timeInMinutes;
 
+// Interrupt Variables
+long lastInterrupt;
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial1.begin(9600);
   SerialUSB.begin(9600);
 
   infoBuffer = "";
+  lastInterrupt = 0;
 
-  pinMode(13, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(52), isr1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(53), isr2, FALLING);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  /*if (variableKey.equals("score1")){
-    score1 = variableValue.toInt();
-    infoBuffer = "";
+  if (digitalRead(52)){
+    digitalWrite(13, HIGH);
   }
-
-  else if (variableKey.equals("score2")){
-    score2 = variableValue.toInt();
-    infoBuffer = "";
+  else{
+    digitalWrite(13, LOW);
   }
-
-  else if (variableKey.equals("uname1")){
-    username1 = variableValue;
-    infoBuffer = "";
-  }
-
-  else if (variableKey.equals("uname2")){
-    username2 = variableValue;
-    infoBuffer = "";
-  }
-
-  else if (variableKey.equals("time")) {
-    timeInMinutes = variableValue.toInt();
-    infoBuffer = "";
-  }
-
-  else {
-    infoBuffer = "";
-  }*/
 }
 
 void serialEvent1(){
@@ -119,6 +105,24 @@ void setVals (){
   SerialUSB.println(username1);
   SerialUSB.println(username2);
   SerialUSB.println(timeInMinutes);
+}
+
+void isr1 () {
+  if (millis() - lastInterrupt > 2500){
+    score1 ++;
+    //Send Score Here
+    SerialUSB.println(score1);
+    lastInterrupt = millis();
+  }
+}
+
+void isr2 () {
+  if (millis() - lastInterrupt > 2500){
+    score2 ++;
+    //Send Score Here
+    SerialUSB.println(score2);
+    lastInterrupt = millis();
+  }
 }
 
 // Verify the syntax of the incoming string
