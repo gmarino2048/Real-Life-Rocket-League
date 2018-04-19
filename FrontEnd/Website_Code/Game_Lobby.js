c980bd96ed3a1d951a599ddd56f85b4b297ff49d
BrieLemon <bxl373@case.edu>
2018-04-18 23:22:05 -0400

    Commented Out

--- FrontEnd/Website_Code/Game_Lobby.js
+++ FrontEnd/Website_Code/Game_Lobby.js
@@ -1,6 +1,6 @@
-var userRequest = new Request('http://localhost/data');
+/*var userRequest = new Request('http://localhost/data');
 var params = new URLSearchParams(window.location.search);
-window.localStorage.token = params.get(token);
+window.localStorage.token = params.get(nsc27);
 
 $.ajax({
     url: "http://localhost/data?token=" + window.localStorage.token,
@@ -11,12 +11,11 @@ $.ajax({
     error: function (data) {
         console.log(data);
     }
-});
+});*/
 
-document.getElementById("game").onclick = function () {
-    location.href = "Login_Page.html";
-}
-
-document.getElementById("stats").onclick = function () {
-    location.href = "Analytics.html";
-}
+    document.getElementById("game").onclick = function () {
+        location.href = "Login_Page.html";
+    };
+    document.getElementById("stats").onclick = function () {
+        location.href = "Analytics.html";
+    };
