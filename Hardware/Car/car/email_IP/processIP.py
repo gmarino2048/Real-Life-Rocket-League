89ce89229896869ecdf95725ab334d29eb8307f0
Griffin <35904717+gjbsaiia@users.noreply.github.com>
2018-05-03 17:24:29 -0400

    for loop was tabbed over

--- Hardware/Car/car/email_IP/processIP.py
+++ Hardware/Car/car/email_IP/processIP.py
@@ -12,17 +12,17 @@ def main():
 	with open(filename) as f:
 		lines = f.readlines()
 		f.close()
-		flag = False
-		for line in lines:
-			line = line.split(" ")
-			for word in  line:
-				if(flag):
-					flag = False
-					if(word != "127.0.0.1/8"):
-						ip.append(word)
-				else:
-					if word == "inet":
-						flag = True
+	flag = False
+	for line in lines:
+		line = line.split(" ")
+		for word in  line:
+			if(flag):
+				flag = False
+				if(word != "127.0.0.1/8"):
+					ip.append(word)
+			else:
+				if word == "inet":
+					flag = True
 	#clears file
 	f = open(filename, 'w')
 	for addr in ip:
